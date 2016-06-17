d8a74b005f73559efcff675628309abb1b6dd260
mac <mac@wcw-stud-145-109-9-93.wireless.uva.nl>
2016-06-17 15:41:08 +0200

    17 juni

--- tickets.js
+++ tickets.js
@@ -7,132 +7,107 @@ countries = ["BEL", "BGR", "CZE", "DNK", "DEU", "EST", "IRL", "GRC", "ESP", "FRA
 //to be used variable
 var numbers
 
-
-d3.json("tickets.json", function(error, numbers) {
-  if (error) return console.warn(error);
-
-
-
-  // all variables to be used
-      var tickets = []
-
-      //go through all countries out of json array
-      for (i = 0; i < 28; i++)
-      { 
-            //update variables
-            tickets[countries[i]] = numbers[i];
-      }
-
+// queue()
+//   .defer(d3.json, 'tourism_af.json')
+//   .defer(d3.json, 'koopkrachtquota.json')
 
 
 d3.json("tourism_af.json", function(error, data) {
   if (error) return console.warn(error);
 
-  // all variables to be used
-      var out_tourism = []
-      var in_tourism = []
+d3.json("koopkrachtquota-af.json", function(error, numbers) {
+  if (error) return console.warn(error);
 
-      //go through all countries out of json array
-      for (i = 0; i < 28; i++)
-      { 
-            //update variables
-            out_tourism[countries[i]] = data[i];
-      }
+// all variables to be used
+var out_tourism = []
+var in_tourism = []
 
-      //go through all countries out of json array
-      for (i = 28; i < data.length; i++)
-      { 
-            //update variables
-            in_tourism[countries[i - 28]] = data[i];
-      }
-
-
-
-for (i = 0; i < countries.length; i++) {
-  for (j = 0; j < countries.length; j++) {
-    //make right datastructure
-    temp = in_tourism[countries[i]][countries[j]]
-    in_tourism[countries[i]][countries[j]] = {ticket: temp}
-    checker = +(in_tourism[countries[i]][countries[j]].ticket)
-    
-    //append the fillkey
-    
-    if (checker > 10000000){
-      in_tourism[countries[i]][countries[j]]["fillKey"] = '7';
-    }
-    else if (checker > 5000000)
-      in_tourism[countries[i]][countries[j]]["fillKey"] = '6';
-    else if (checker > 2500000)
-      in_tourism[countries[i]][countries[j]]["fillKey"] = '5';
-    else if (checker > 1000000)
-      in_tourism[countries[i]][countries[j]]["fillKey"] = '4';
-    else if (checker > 800000)
-      in_tourism[countries[i]][countries[j]]["fillKey"] = '3';
-    else if (checker > 400000)
-      in_tourism[countries[i]][countries[j]]["fillKey"] = '2';
-    else if (checker > 400000)
-      in_tourism[countries[i]][countries[j]]["fillKey"] = '1';
-    else if (checker > 1)
-      in_tourism[countries[i]][countries[j]]["fillKey"] = '0';
-  }
+//go through all countries out of json array
+for (i = 0; i < 28; i++)
+{ 
+      //update variables
+      out_tourism[countries[i]] = data[i];
 }
 
-for (i = 0; i < countries.length; i++) {
-  for (j = 0; j < countries.length; j++) {
-    //make right datastructure
-    temp = out_tourism[countries[i]][countries[j]]
-    out_tourism[countries[i]][countries[j]] = {ticket: temp}
-    checker = +(out_tourism[countries[i]][countries[j]].ticket)
-    
-    //append the fillkey
-    
-    if (checker > 10000000){
-      out_tourism[countries[i]][countries[j]]["fillKey"] = '7';
-    }
-    else if (checker > 5000000)
-      out_tourism[countries[i]][countries[j]]["fillKey"] = '6';
-    else if (checker > 2500000)
-      out_tourism[countries[i]][countries[j]]["fillKey"] = '5';
-    else if (checker > 1000000)
-      out_tourism[countries[i]][countries[j]]["fillKey"] = '4';
-    else if (checker > 800000)
-      out_tourism[countries[i]][countries[j]]["fillKey"] = '3';
-    else if (checker > 400000)
-      out_tourism[countries[i]][countries[j]]["fillKey"] = '2';
-    else if (checker > 400000)
-      out_tourism[countries[i]][countries[j]]["fillKey"] = '1';
-    else if (checker > 1)
-      out_tourism[countries[i]][countries[j]]["fillKey"] = '0';
-  }
+//go through all countries out of json array
+for (i = 28; i < data.length; i++)
+{ 
+      //update variables
+      in_tourism[countries[i - 28]] = data[i];
+}
+
+var quota_in = []
+var quota_out = []
+// all variables to be used
+//go through all countries out of json array
+for (i = 0; i < 28; i++)
+{ 
+      //update variables
+      quota_in[countries[i]] = numbers[i];
+}
+
+for (i = 0; i < 56; i++)
+{ 
+      //update variables
+      quota_out[countries[i]] = numbers[i + 28];
 }
 
 
 
+//reforme data
+dataFormat(in_tourism, 'in');
+dataFormat(out_tourism, 'out');
+
+function dataFormat(data, prefix){
+  for (i = 0; i < countries.length; i++) {
+    for (j = 0; j < countries.length; j++) {
+      //make right datastructure
+      temp = data[countries[i]][countries[j]]
+      data[countries[i]][countries[j]] = {ticket: temp}
+      checker = +(data[countries[i]][countries[j]].ticket)
+      
+      //append the fillkey
+      if (checker > 10000000){
+        data[countries[i]][countries[j]]["fillKey"] = prefix + '7';
+      }
+      else if (checker > 5000000)
+        data[countries[i]][countries[j]]["fillKey"] = prefix + '6';
+      else if (checker > 2500000)
+        data[countries[i]][countries[j]]["fillKey"] = prefix + '5';
+      else if (checker > 1000000)
+        data[countries[i]][countries[j]]["fillKey"] = prefix + '4';
+      else if (checker > 800000)
+        data[countries[i]][countries[j]]["fillKey"] = prefix + '3';
+      else if (checker > 400000)
+        data[countries[i]][countries[j]]["fillKey"] = prefix + '2';
+      else if (checker > 200000)
+        data[countries[i]][countries[j]]["fillKey"] = prefix + '1';
+      else if (checker > 0)
+        data[countries[i]][countries[j]]["fillKey"] = prefix + '0';
+      else
+        data[countries[i]][countries[j]]["fillKey"] = 'none'
+    }
+  }
+};  
+
+//incoming or outgoing to show
 in_or_out = 0;
 
-// button = document.getElementById('button')
-// console.log(button)
-// button.onclick
-country_picked = 0;
-//button 
-d3.select('#button').on('click', function(geography) {
-  if (in_or_out === 0) {
-    graph.updateChoropleth(in_tourism[country_picked]);
-    makePieChart(country_picked)
-    in_or_out = 1;
-  }
-  else {    
-    graph.updateChoropleth(out_tourism[country_picked]);
-    makePieChart(country_picked)
-    in_or_out = 0;
-  }
-})
+textInUit(in_or_out)
 
-//dropdown
-d3.selectAll('.dropdown-menu li').on('click', function(d){
-   var data = d3.select(this).property('value');
-   console.log(data)
-})
+//text in- of uitstroom
+function textInUit(in_or_out) {
+  if (in_or_out == 0)
+    document.getElementById("in_of_uit").innerHTML = "De instroom van toeristen"
+  else {
+    document.getElementById("in_of_uit").innerHTML = "De uitstroom van toeristen"
+  }
+};
+
+//which country was picked
+country_picked = 0;
+
+
 
 
 
@@ -153,23 +128,34 @@ var graph = new Datamap({
           geographyConfig: {
             highlightBorderColor: '#bada55',
             popupTemplate: function(geography, data) {
-              return '<div class="hoverinfo"><b>' + geography.properties.name + '</b></br> Tourism: ' +  data.ticket + ' '
+              return '<div class="hoverinfo"><b>' + geography.properties.name + '</b></br> Tourism: ' +  Math.round(data.ticket/100000, 2) + ' '
             },
           highlightBorderWidth: 3
           },
           //fill scale
-          fills: {
-              '0' : "#ece7f2",
-              '1' : "#d0d1e6",
-              '2' : '#a6bddb',
-              '3' : '#74a9cf',
-              '4' : '#3690c0',
-              '5' : '#0570b0',
-              '6' : '#045a8d',
-              '7' : '#023858',
-              defaultFill: 'grey' 
+          fills: {  
+                'in0' : "#ece7f2",
+                'in1' : "#d0d1e6",
+                'in2' : '#a6bddb',
+                'in3' : '#74a9cf',
+                'in4' : '#3690c0',
+                'in5' : '#0570b0',
+                'in6' : '#045a8d',
+                'in7' : '#023858',
+             
+                'out0' : "#fee0d2",
+                'out1' : "#fcbba1",
+                'out2' : '#fc9272',
+                'out3' : '#fb6a4a',
+                'out4' : '#ef3b2c',
+                'out5' : '#cb181d',
+                'out6' : '#a50f15',
+                'out7' : '#67000d',
+                'defaultFill': 'grey',
+                'none' : 'grey'
            },
           data: in_tourism[countries[0]],
+          paintBorder: 'BEL',          
            //when a country is clicked, call function MakeBarChart
            done: function(datamap) {
               datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
@@ -179,6 +165,7 @@ var graph = new Datamap({
               else
                 graph.updateChoropleth(out_tourism[geography.id]);
               makePieChart(geography.id)
+              makeScatterChart(geography.id)
               country_picked = geography.id
               //give highest prices
               // makeBarChart()
@@ -187,8 +174,13 @@ var graph = new Datamap({
     });
 
 
+//Legend
 
-makePieChart("BEL", in_tourism)
+
+
+
+
+//PieChart
 
 function makePieChart(id) {
   
@@ -209,32 +201,57 @@ function makePieChart(id) {
     return parseFloat(b.ticket) - parseFloat(a.ticket);
   })
 
-
-
   var data = []
   var summer = 0;
   for (i = 0; i < 7; i++) {
-    data.push(data2[i]);
+    if (data2[i].ticket != 0)   
+      data.push(data2[i]);
   }
   for (i = 7; i < 28; i++){
     summer = summer + data2[i].ticket;
   }
-  data.push({"country": 'remaining', "ticket": summer})
+  data.push({"country": 'remain', "ticket": summer})
+
+  console.log(data)
+
+  if (data.length < 2) {
+    console.log('no pie')
+    document.getElementById('text-piechart').innerHTML = 'No Data to show';
+  }
+  else {
+    document.getElementById('text-piechart').innerHTML = '';
+  temp1 = data[0]
+  temp2 = data[2]
+  temp3 = data[4]
+
+  data[0] = data[7]
+  data[7] = temp1
+  data[2] = data[5]
+  data[5] = temp2
+  data[4] = data[3]
+  data[3] = temp3
 
   var width = 960,
       height = 500,
-      radius = Math.min(width, height) / 3;
+      radius = Math.min(width, height) / 4;
 
-  var color = d3.scale.ordinal()
-      .range(["#eff3ff", "#c6dbef", "#084594", "#6baed6", "#9ecae1", "#4292c6", "#2171b5", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#084594", "#eff3ff", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#eff3ff", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#084594",]);
+  if (in_or_out == 0) {
+    var b = ["#9ecae1", "#6baed6", "#eff3ff", "#c6dbef", "#2171b5", "#4292c6", "#c6dbef", "#9ecae1", "#4292c6", "#6baed6", "#084594", "#eff3ff", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#eff3ff", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#084594",];
+  }
+  else {
+    var b = ["#fee0d2", '#fc9272', '#ef3b2c', '#a50f15', '#fb6a4a', '#cb181d', '#a50f15', "#fcbba1", '#67000d', "#fcbba1", "#4292c6", "#2171b5", "#084594", "#eff3ff", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#eff3ff", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#084594",];
+  }
 
+  var color = d3.scale.ordinal() 
+      .range(b);
+    
   var arc = d3.svg.arc()
       .outerRadius(radius - 10)
-      .innerRadius(40);
+      .innerRadius(90);
 
   var labelArc = d3.svg.arc()
       .outerRadius(radius + 50)
-      .innerRadius(radius - 30);
+      .innerRadius(radius - 0);
 
   var pie = d3.layout.pie()
       .sort(null)
@@ -247,10 +264,6 @@ function makePieChart(id) {
     .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  // d3.csv("data.csv", type, function(error, data) {
-  //   if (error) throw error;
-   
-
     var g = svg.selectAll(".arc")
         .data(pie(data))
       .enter().append("g")
@@ -262,24 +275,226 @@ function makePieChart(id) {
 
     g.append("text")
         .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
-        .attr("dy", ".85em")
+        .attr("dy", "1em")
         .text(function(d) { return d.data.country; });
- 
-
-  function type(d) {
-    d.population = +d.population;
-    return d;
   }
 }
 
-function makeBarChart(){
 
 
 
-}
 
 
 
+
+//ScatterChart
+
+function makeScatterChart(id){
+
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = 400 - margin.left - margin.right,
+      height = 230 - margin.top - margin.bottom;
+
+  // /* 
+  //  * value accessor - returns the value to encode for a given data object.
+  //  * scale - maps value to a visual display encoding, such as a pixel position.
+  //  * map function - maps from data value to display value
+  //  * axis - sets up axis
+  //  */ 
+
+  var quotum = [];
+
+  if (in_or_out == 0)
+    var quota2 = quota_in[id];
+  else
+    var quota2 = quota_out[id];
+
+  d3.select("#scatter").remove()
+
+  //MAKE ALL DATA READY
+  if (in_or_out == 0)
+    var country_ticket = in_tourism[id]
+  else 
+    var country_ticket = out_tourism[id]
+  
+  
+  //push everything to quotum
+  for (var key in country_ticket)
+  { 
+    //update variables
+    if (+quota2[key] != 0 && +country_ticket[key].ticket != 0)
+      quotum.push({"country" : key, "quotum" : +quota2[key], "tourists" : +country_ticket[key].ticket/1000000});
+  }
+
+  //variable to make scatter
+  var data = quotum;
+
+
+   //x quotum, y tourists
+  // setup x 
+  var xValue = function(d) { return d.quotum;}, // data -> value
+      xScale = d3.scale.linear().range([0, width]), // value -> display
+      xMap = function(d) { return xScale(xValue(d));}, // data -> display
+      xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+  // setup y
+  var yValue = function(d) { return d.tourists;}, // data -> value
+      yScale = d3.scale.linear().range([height, 0]), // value -> display
+      yMap = function(d) { return yScale(yValue(d));}, // data -> display
+      yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+  // add the graph canvas to the body of the webpage
+  var svg = d3.select("#container5").append("svg")
+      .attr("id", "scatter")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // add the tooltip area to the webpage
+  var tooltip = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+
+    var xLabels = [d3.min(data, xValue), d3.max(data, xValue)]
+    // don't want dots overlapping axis, so add in buffer to data domain
+    xScale.domain(xLabels);
+    yScale.domain([d3.min(data, yValue), d3.max(data, yValue)]);
+
+    // x-axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("koopkrachtquotum");
+
+    // y-axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Toeristen Aantal ");
+
+    // draw dots
+    svg.selectAll(".dot")
+        .data(data)
+      .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", xMap)
+        .attr("cy", yMap)
+        .on("mouseover", function(d) {
+            console.log('mouse')
+            tooltip.transition()
+                 .duration(200)
+                 .style("opacity", .9);
+            tooltip.html(d.country + "<br/> (" + xValue(d) 
+            + ", " + yValue(d) + ")")
+                 .style("left", (d3.event.pageX + 20) + "px")
+                 .style("top", (d3.event.pageY - 50) + "px");
+        })
+        .on("mouseout", function(d) {
+            tooltip.transition()
+                 .duration(500)
+                 .style("opacity", 0);
+        });
+
+
+  // returns slope, intercept and r-square of the line
+  function leastSquares(xSeries, ySeries) {
+    var reduceSumFunc = function(prev, cur) { return prev + cur; };
+    
+    var xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
+    var yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
+    var ssXX = xSeries.map(function(d) { return Math.pow(d - xBar, 2); })
+      .reduce(reduceSumFunc);
+    
+    var ssYY = ySeries.map(function(d) { return Math.pow(d - yBar, 2); })
+      .reduce(reduceSumFunc);
+      
+    var ssXY = xSeries.map(function(d, i) { return (d - xBar) * (ySeries[i] - yBar); })
+      .reduce(reduceSumFunc);
+      
+    var slope = ssXY / ssXX;
+    var intercept = yBar - (xBar * slope);
+    var rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
+    
+    return [slope, intercept, rSquare];
+  }
+
+
+
+  var xSeries = d3.range(1, xLabels.length + 1)
+  var ySeries = data.map(function(d) { return parseFloat(d.tourists); });
+  var leastSquaresCoeff = leastSquares(xSeries, ySeries);
+  // apply the reults of the least squares regression
+  var x1 = xLabels[0];
+  var y1 = leastSquaresCoeff[0]*xSeries[0] + leastSquaresCoeff[1];
+  var x2 = xLabels[xLabels.length - 1];
+  var y2 = leastSquaresCoeff[0] * xSeries[xSeries.length - 1] + leastSquaresCoeff[1];
+  var trendData = [[x1,y1,x2,y2]];
+  var trendline = svg.selectAll(".trendline")
+  .data(trendData);
+  trendline.enter()
+    .append("line")
+    .attr("class", "trendline")
+    .attr("x1", function(d) { console.log(d[0]); return xScale(d[0])})
+    .attr("y1", function(d) { return yScale(d[1]); })
+    .attr("x2", function(d) { return xScale(d[2]); })
+    .attr("y2", function(d) { return yScale(d[3]); })
+    .attr("stroke", "red")
+    .attr("stroke-width", 1.5);
+
+};
+
+
+
+
+
+
+
+//make first Piechart en ScatterChart
+
+makePieChart("BEL", in_tourism)
+makeScatterChart('BEL')
+country_picked = 'BEL'
+
+
+//button 
+d3.select('#button').on('click', function(geography) {
+  if (in_or_out == 0) {
+    in_or_out = 1;
+    graph.updateChoropleth(out_tourism[country_picked]);
+    textInUit(in_or_out);
+    makePieChart(country_picked);
+    makeScatterChart(country_picked);
+    
+  }
+  else {    
+    in_or_out = 0;
+    graph.updateChoropleth(in_tourism[country_picked]);
+    textInUit(in_or_out)
+    makePieChart(country_picked)
+    makeScatterChart(country_picked)
+  }
+})
+
+//dropdown
+d3.selectAll('.dropdown-menu li').on('click', function(d){
+   var data = d3.select(this).property('value');
+   // console.log(data)
+})
 
 
 
